1a39cbbaab54142dc36c892a75be1621d17194a1
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-04-24 17:42:47 +0530

    initialized

---
+++ script1.js
@@ -0,0 +1,14 @@
+function solve(val){
+    var v=document.getElementById('out');
+    v.value +=val;
+
+}
+function result(){
+    var num=document.getElementById('out').value
+    var num1 =eval(num);
+    document.getElementById('out').value=num1;
+}
+
+function clear(){
+    document.getElementById('out').value=" ";
+}
